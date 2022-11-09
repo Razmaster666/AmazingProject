d35b234462ebe37eb754964e4cfbe6e78bf4af2e
RasmusClausen <rasmusclausen123@gmail.com>
2022-11-09 10:37:05 +0100

    Changed the for loop to count down instead

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int num = 10;
 
-        for (int i = 1; i <= num; i++){
+        for (int i = num; i >= 1; i--){
             System.out.println(i);
         }
     }
